eec9a89da3d4cefe2ac4a525bf1df79116b830cc
Joshlete <joshgiurculete@yahoo.com>
2024-04-18 14:49:47 -0700

    Added user login and logout

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("job/<int:pk>", views.job, name="job"),
+    path("login/", views.loginUser, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerUser, name="register"),
 ]
